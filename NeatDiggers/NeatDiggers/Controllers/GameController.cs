ecc164ae2cec997f90d0e3219e503196db4e8895
buzzon <buzzondev@gmail.com>
2020-12-04 12:41:17 +0500

    Add Game,Wiki Controllers. Update navbar

---
+++ NeatDiggers/NeatDiggers/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeatDiggers.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Matches()
+        {
+            return View();
+        }
+    }
+}
---
+++ NeatDiggers/NeatDiggers/Controllers/WikiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace NeatDiggers.Controllers
+{
+    public class WikiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string query)
+        {
+            ViewData["Query"] = query;
+            return View();
+        }
+    }
+}
