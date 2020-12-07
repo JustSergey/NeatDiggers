55ca392fddfbd34277fc1a0c016826a285420368
buzzon <buzzondev@gmail.com>
2020-12-07 15:36:59 +0500

    Client SignalR Watch

---
+++ NeatDiggers/NeatDiggers/wwwroot/js/lobby.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/GameHub")
+    .build();
+
+connection.start().then(function () {
+    var code = document.getElementById("code").innerText;
+    console.log(code);
+    connection.invoke("ConnectToRoomAsSpectator", code).catch(function (err) {
+        return console.error(err.toString());
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("ChangeState", function (room) {
+    var li = document.createElement("li");
+    li.textContent = "ChangeState";
+    document.getElementById("SpectatorsList").appendChild(li);
+});
+
+connection.on("ChangeStateWithAction", function (room, gameAction) {
+    var li = document.createElement("li");
+    li.textContent = "ChangeStateWithAction";
+    document.getElementById("SpectatorsList").appendChild(li);
+});
